7af62ad1a713a2cc329d1cc012d118234de66880
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-25 20:33:44 -0300

    Add tsup.config.ts file

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  format: ["cjs", "esm"],
+  external: ["express"],
+  target: "es2022",
+  sourcemap: true,
+  minify: "terser",
+  terserOptions: {
+    compress: true,
+  },
+  entry: {
+    index: "./src/index.ts",
+  },
+});
