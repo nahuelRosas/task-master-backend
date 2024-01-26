f69c20150a1cfd666a9b42f30cd0e5b5116d9f6d
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 06:18:42 -0300

    Add getTask controller and update routes

--- src/routes/tasks.route.ts
+++ src/routes/tasks.route.ts
@@ -1,8 +1,19 @@
+import { getTask } from "@/controllers/tasks";
 import { validateToken } from "@/middlewares/validate-token";
 import { Router } from "express";
 const router = Router();
 
-router.get("/tasks", validateToken, () => {
+router.get("/tasks", validateToken, getTask);
+router.get("/tasks/:id", validateToken, () => {
+  console.log("tasks");
+});
+router.post("/tasks", validateToken, () => {
+  console.log("tasks");
+});
+router.delete("/tasks/:id", validateToken, () => {
+  console.log("tasks");
+});
+router.put("/tasks/:id", validateToken, () => {
   console.log("tasks");
 });
 
