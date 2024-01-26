d51d0c8bd7059aa3ff1f9e9db82e19a31eab3cc3
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 06:07:02 -0300

    Add tasks route to index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,25 +1,10 @@
 import profile from "./profile.route";
+import tasks from "./tasks.route";
 import { Router } from "express";
 import auth from "./auth.route";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  const html = `
-<html>
-    <head>
-      <title>Technical Test Crud</title>
-    </head>
-    <body>
-        <h1>Technical Test Crud - Nahuel Rosas</h1>
-        <p>API</p>
-        <ul>
-            <li><a href="/api/profile">Profile</a></li>
-        </ul>
-    </body>
-</html>`;
-  res.send(html);
-});
-router.use("/api", auth, profile);
+router.use("/api", auth, profile, tasks);
 
 export default router;
