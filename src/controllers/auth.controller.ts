21888f18481c5a88ee8b611d74601d69f8e93338
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 00:53:03 -0300

    Add logging for registration data

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from "express";
 
 export function register(req: Request, res: Response) {
+  const { email, password, username } = req.body;
+  console.log({
+    email,
+    password,
+    username,
+  });
   res.send("register");
 }
 
