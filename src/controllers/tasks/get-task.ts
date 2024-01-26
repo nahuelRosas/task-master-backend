06723ca71f3ec5c94e5b9ce1d628e1d6042b7986
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 06:40:32 -0300

    Add task retrieval functionality

--- src/controllers/tasks/get-task.ts
+++ src/controllers/tasks/get-task.ts
@@ -2,6 +2,7 @@ import { RequestWithUser } from "@/types/globals";
 import { logInfo } from "@/libs/log-info";
 import { Response } from "express";
 import validateUser from "@/middlewares/validate-user";
+import Task from "@/models/task.model";
 
 export async function getTask(
   req: RequestWithUser,
@@ -9,14 +10,18 @@ export async function getTask(
 ): Promise<void> {
   try {
     const user = await validateUser(req, res);
-    if (!user) {
-      return;
+    if (user) {
+      const task = await Task.findOne({ _id: req.params.id, owner: user._id });
+      if (!task) {
+        res.status(404).send("Task not found");
+        return;
+      }
+      res.status(200).json(task);
     }
-    console.log(user);
   } catch (error) {
     if (error instanceof Error) {
       logInfo({
-        logMessage: `Error getting profile: ${error.message}`,
+        logMessage: `Error getting Task: ${error.message}`,
         logType: "error",
       });
       res.status(500).send(error.message);
