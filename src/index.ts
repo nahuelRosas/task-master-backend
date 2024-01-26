83e47469a96db07206cdf62177dd3086a3e49471
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-25 22:21:47 -0300

    Add database connection and ping endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import connectDataBase from "./database";
 import express from "express";
 import app from "./server";
+
+// Connect to the database.
+connectDataBase();
 
 app.get("/ping", (req: express.Request, res: express.Response) => {
   res.send("pong");
