95152abb7f7b239980cd6c9dbbd1d76b7a81fca0
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 01:06:13 -0300

    Add user registration functionality with error handling

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,13 +1,24 @@
 import { Request, Response } from "express";
-
-export function register(req: Request, res: Response) {
+import User from "@/models/user.model";
+import { logInfo } from "@/libs/logInfo";
+export async function register(req: Request, res: Response) {
   const { email, password, username } = req.body;
-  console.log({
-    email,
-    password,
-    username,
-  });
-  res.send("register");
+  try {
+    const user = new User({ email, password, username });
+    await user.save();
+    logInfo({
+      logMessage: `User ${user.username} created`,
+      logType: "success",
+    });
+    res.send(`User ${user.username} created`);
+  } catch (error) {
+    logInfo({
+      logMessage: `Error creating user: ${error}`,
+      logType: "error",
+    });
+
+    res.status(500).send(error);
+  }
 }
 
 export function login(req: Request, res: Response) {
