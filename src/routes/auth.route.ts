5f633a05ccc3274f402c313ac8342ba5785826e3
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 00:31:18 -0300

    Refactor auth routes to use controller functions

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,6 @@
 import { Router } from "express";
-
+import { login, register } from "@/controllers/auth.controller";
 const router = Router();
 
-router.post("/login", (req, res) => {
-  res.send("login");
-});
-
-router.post("/register", (req, res) => {
-  res.send("register");
-});
+router.post("/login", login);
+router.post("/register", register);
