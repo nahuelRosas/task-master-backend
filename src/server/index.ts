cf5f0d91e336ee1d9a47cad8db3cdef9a4a212bf
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-25 21:21:35 -0300

    Add middleware for logging, security, and request parsing

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,16 +1,69 @@
-import { logInfo } from "@/libs/logInfo";
-import * as dotenv from "dotenv";
+import { IncomingMessage, ServerResponse } from "node:http";
+import morgan, { TokenIndexer } from "morgan";
 import express, { Express } from "express";
+import cookieParser from "cookie-parser";
+import { logInfo } from "@/libs/logInfo";
+import compression from "compression";
+import bodyParser from "body-parser";
+import * as dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
 
 dotenv.config();
-const { PORT } = process.env;
 
-/**
- * Express application instance.
- */
+const { PORT } = process.env;
 const app: Express = express();
 
-app.use(express.json());
+// Morgan Middleware Configuration
+function configureMorganOptions(
+  tokens: TokenIndexer,
+  req: IncomingMessage,
+  res: ServerResponse
+): null | undefined {
+  if (!tokens) return null;
+
+  const status = tokens?.status?.(req, res);
+  const response = `
+    Remote Addr: ${tokens?.["remote-addr"]?.(req, res)}
+    Remote User: ${tokens?.["remote-user"]?.(req, res)}
+    Method: ${tokens?.method?.(req, res)}
+    URL: ${tokens?.url?.(req, res)}
+    HTTP Version: ${tokens?.["http-version"]?.(req, res)}
+    Status: ${status}
+    Response Time: ${tokens["response-time"]?.(req, res)}ms
+    User-Agent:${tokens["user-agent"]?.(req, res)}`;
+
+  if (status) {
+    const logType =
+      typeof status === "undefined" || +status >= 400 ? "error" : "info";
+    logInfo({
+      logMessage: response,
+      filename: "MORGAN-MIDDLEWARE",
+      logType,
+    });
+  }
+
+  return null;
+}
+
+// Middleware Setup
+app.use(
+  morgan(configureMorganOptions),
+  helmet(),
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+  express.json(),
+  express.urlencoded({ extended: true }),
+  bodyParser.json({ limit: "50mb" }),
+  bodyParser.urlencoded({ limit: "50mb", extended: true }),
+  cookieParser(),
+  compression()
+);
+
+// Server Setup
 export const server = app.listen(PORT || 3000, () => {
   logInfo({
     logMessage: `Server running on port ${PORT}`,
