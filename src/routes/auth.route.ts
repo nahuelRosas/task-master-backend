7a52b5f3028afe40d0aae40d7fe502d9d0a56c71
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 07:45:32 -0300

    Update auth route to include validation middleware

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,11 @@
+import { validateRegisterUser } from "@/middlewares/validate-auth-user";
+import { registerSchema, loginSchema } from "@/schemas/auth.schema";
 import { login, register, logout } from "@/controllers/auth";
 import { Router } from "express";
 const router = Router();
 
-router.post("/register", register);
-router.post("/logout", logout);
+router.post("/register", validateRegisterUser(registerSchema), register);
+router.post("/logout", validateRegisterUser(loginSchema), logout);
 router.post("/login", login);
 
 export default router;
