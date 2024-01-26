ca628d7880d6bd123543a437f0722174f3294da3
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 03:16:36 -0300

    Add homepage route to index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,22 @@ import auth from "./auth.route";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+  const html = `
+<html>
+    <head>
+      <title>Technical Test Crud</title>
+    </head>
+    <body>
+        <h1>Technical Test Crud</h1>
+        <p>API</p>
+        <ul>
+            <li><a href="/api/profile">Profile</a></li>
+        </ul>
+    </body>
+</html>`;
+  res.send(html);
+});
 router.use("/api", auth, profile);
 
 export default router;
