a76a68c7b6f76d903cbd27f76e73acfadeb5e413
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 07:54:34 -0300

    Add console log for error in validateRegisterUser middleware

--- src/middlewares/validate-auth-user.ts
+++ src/middlewares/validate-auth-user.ts
@@ -7,6 +7,7 @@ export function validateRegisterUser(schema: Schema) {
   return function (req: RequestWithUser, res: Response, next: NextFunction) {
     try {
       const { error } = schema.parse(req.body);
+      console.log({ error });
       if (error) {
         logInfo({
           logMessage: `Error validating user: ${error.message}`,
