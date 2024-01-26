d59faaf2e24ea9f30272d43651eb433a90a9f3b3
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 05:54:29 -0300

    Refactor profile route to use getProfile controller

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -1,9 +1,8 @@
 import { validateToken } from "@/middlewares/validate-token";
+import { getProfile } from "@/controllers/profile/";
 import { Router } from "express";
 const router = Router();
 
-router.get("/profile", validateToken, (req, res) => {
-  res.status(200).send("Profile");
-});
+router.get("/profile", validateToken, getProfile);
 
 export default router;
