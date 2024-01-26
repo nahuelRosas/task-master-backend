48c69571dd800a85565d96e526b89b6ef8f158e0
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 02:49:10 -0300

    Add token validation to profile route

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -1,6 +1,9 @@
+import { validateToken } from "@/middlewares/validate-token";
 import { Router } from "express";
 const router = Router();
 
-router.get("/profile", (req, res) => {});
+router.get("/profile", validateToken, (req, res) => {
+  res.status(200).send("Profile");
+});
 
 export default router;
