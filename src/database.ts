89031ae142c632b6d114e56172eb89a01c652d49
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-25 22:21:39 -0300

    Add database connection code

---
+++ src/database.ts
@@ -0,0 +1,30 @@
+import { logInfo } from "./libs/logInfo";
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
+const { URL_MONGO } = process.env;
+/**
+ * Connects to the database.
+ * @returns A Promise that resolves to void.
+ */
+async function connectDataBase(): Promise<void> {
+  try {
+    const data = await mongoose.connect(
+      `${URL_MONGO || "mongodb://localhost:27017"}`
+    );
+    logInfo({
+      logMessage: `MONGO DB CONECTED WITH SERVER ${data.connection.host.toUpperCase()}`,
+      filename: __filename,
+      logType: "success",
+    });
+  } catch (data) {
+    logInfo({
+      logMessage: `Error ${data}`,
+      filename: __filename,
+      logType: "error",
+    });
+  }
+}
+
+export default connectDataBase;
