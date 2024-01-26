01cba94320157c807697dd3c37da51e74f169524
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 06:44:27 -0300

    Add deleteTask function to delete a task from the database

--- src/controllers/tasks/delete-task.ts
+++ src/controllers/tasks/delete-task.ts
@@ -2,17 +2,34 @@ import { RequestWithUser } from "@/types/globals";
 import { logInfo } from "@/libs/log-info";
 import { Response } from "express";
 import validateUser from "@/middlewares/validate-user";
+import Task from "@/models/task.model";
 
+/**
+ * Deletes a task from the database.
+ *
+ * @param req - The request object containing user information.
+ * @param res - The response object used to send the HTTP response.
+ * @returns A Promise that resolves to void.
+ */
 export async function deleteTask(
   req: RequestWithUser,
   res: Response
 ): Promise<void> {
   try {
     const user = await validateUser(req, res);
-    if (!user) {
-      return;
+    if (user) {
+      const task = await Task.findOneAndDelete({
+        _id: req.params.id,
+        owner: user._id,
+      });
+
+      if (!task) {
+        res.status(404).send("Task not found");
+        return;
+      }
+
+      res.status(200).json(task);
     }
-    console.log(user);
   } catch (error) {
     if (error instanceof Error) {
       logInfo({
