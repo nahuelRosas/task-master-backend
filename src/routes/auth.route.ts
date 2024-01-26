267e4bb030826ac9476f0cf7299ddcb8fcaf6fc4
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 00:40:21 -0300

    Refactor auth.route.ts to import Router from express

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,6 +1,8 @@
-import { Router } from "express";
 import { login, register } from "@/controllers/auth.controller";
+import { Router } from "express";
 const router = Router();
 
 router.post("/login", login);
 router.post("/register", register);
+
+export default router;
