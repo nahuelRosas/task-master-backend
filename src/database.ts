78817da796138da205e72a9067638c260e2aec75
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 05:03:38 -0300

    Refactor database connection and error handling

--- src/database.ts
+++ src/database.ts
@@ -3,16 +3,15 @@ import * as dotenv from "dotenv";
 import mongoose from "mongoose";
 dotenv.config();
 
-const { URL_MONGO } = process.env;
+const { MONGODB_URI } = process.env;
 /**
  * Connects to the database.
  * @returns A Promise that resolves to void.
  */
 async function connectDataBase(): Promise<void> {
   try {
-    const data = await mongoose.connect(
-      `${URL_MONGO || "mongodb://localhost:27017"}`
-    );
+    if (!MONGODB_URI) throw new Error("MONGODB_URI is not defined");
+    const data = await mongoose.connect(MONGODB_URI);
     logInfo({
       logMessage: `MONGO DB CONECTED WITH SERVER ${data.connection.host.toUpperCase()}`,
       filename: __filename,
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-// import connectDataBase from "./database";
+import connectDataBase from "./database";
 import { logInfo } from "./libs/log-info";
 import app from "./server";
 
 const { PORT } = process.env;
 
-// connectDataBase()
-//   .then(() => {
-app.listen(PORT || 3000, () =>
-  logInfo({
-    logMessage: `Server running on port ${PORT || 3000}`,
-    logType: "success",
+connectDataBase()
+  .then(() => {
+    app.listen(PORT || 3000, () =>
+      logInfo({
+        logMessage: `Server running on port ${PORT || 3000}`,
+        logType: "success",
+      })
+    );
   })
-);
-// })
-// .catch((err) => {
-//   logInfo({
-//     logMessage: `Error connecting to database: ${err}`,
-//     logType: "error",
-//   });
-// });
+  .catch((err) => {
+    logInfo({
+      logMessage: `Error connecting to database: ${err}`,
+      logType: "error",
+    });
+  });
