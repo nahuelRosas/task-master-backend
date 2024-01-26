6bf9f867eed2eef4167c9a3e3712997464f976b2
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-26 06:00:30 -0300

    Add RequestWithUser interface to globals.ts

--- src/types/globals.ts
+++ src/types/globals.ts
@@ -1,6 +1,9 @@
 import { JwtPayload } from "jsonwebtoken";
 import { Request } from "express";
 
+/**
+ * Represents an HTTP request with an optional user property.
+ */
 export interface RequestWithUser extends Request {
   user?: JwtPayload | string;
 }
