8a2088be58f44347b48b5dc1097df8971e95ecc9
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-02-06 23:46:32 -0300

    Add express-rate-limit middleware to server

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,9 +8,14 @@ import { csrf } from "lusca";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app: Express = express();
 app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  }),
   morgan(configureMorganOptions),
   helmet(),
   cors({
